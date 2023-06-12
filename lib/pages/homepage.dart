67ef9786e33ae2c971a68e4320bee639d9499585
0x1m4o <chuntdevnx@gmail.com>
2023-06-12 10:06:54 +0700

    Create and use local storage

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:hive/hive.dart';
+
+class HomePage extends StatelessWidget {
+  Box box;
+  HomePage({
+    Key? key,
+    required this.box,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text(box.get('name'))),
+    );
+  }
+}
